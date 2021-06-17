516811a27c52922026290d9151381fc9e26672e1
Dima <35377254+dnef@users.noreply.github.com>
2021-06-17 20:08:04 +0500

    Add files via upload

---
+++ src/ru/tandemservice/test/task1/SplitCell.java
@@ -0,0 +1,62 @@
+package ru.tandemservice.test.task1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SplitCell {
+    // разбиение строки на подстроки
+    protected String[] SplitSub(String row) {
+/*      //  если известно что в строках нет символа разделения который используется при replace
+        String string = row.replaceAll("(\\d+)","*$1*");
+        String[] arrString = sim.split("\\*");
+
+*/
+        //если пришла пустая строка, возвращаем пустой массив
+        if (row.equals("")) {
+            return new String[]{""};
+        }
+        //начинаем формирование подстрок
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(row.charAt(0));
+        //List для хранения подстрок (не знаем сколько подстрок будет)
+        List<String> list = new ArrayList<>();
+
+        for (int i = 1; i < row.length(); i++) {
+            //если предыдущий символ и текущий числа то склеиваем строку
+            if (Character.isDigit(row.charAt(i - 1)) && Character.isDigit(row.charAt(i))) {
+                stringBuilder.append(row.charAt(i));
+            } else {
+                //если предыдущий символ и текущий не числа то склеиваем строку
+                if (!Character.isDigit(row.charAt(i - 1)) && !Character.isDigit(row.charAt(i))) {
+                    stringBuilder.append(row.charAt(i));
+                } else {
+                    //если предыдущий символ не число, а текущий число: символы(подстроку) до текущего символа отправляем в list
+                    // и начинаем формировать новую подстроку
+                    if (!Character.isDigit(row.charAt(i - 1)) && Character.isDigit(row.charAt(i))) {
+                        list.add(stringBuilder.toString());
+                        stringBuilder.setLength(0);
+                        stringBuilder.append(row.charAt(i));
+                    } else {
+                        //если предыдущий символ число, а текущий не число: символы(подстроку) до текущего символа отправляем в list
+                        // и начинаем формировать новую подстроку
+                        if (Character.isDigit(row.charAt(i - 1)) && !Character.isDigit(row.charAt(i))) {
+                            list.add(stringBuilder.toString());
+                            stringBuilder.setLength(0);
+                            stringBuilder.append(row.charAt(i));
+                        }
+
+
+                    }
+                }
+            }
+        }
+        //добавляем в list последнюю подстроку в входной строке
+        list.add(stringBuilder.toString());
+        //создаем массив подстрок
+        String[] cell = list.toArray(new String[list.size()]);
+
+        return cell;
+    }
+}
+
+
---
+++ src/ru/tandemservice/test/task1/Task1Impl.java
@@ -0,0 +1,29 @@
+package ru.tandemservice.test.task1;
+
+import java.util.*;
+
+/**
+ * <h1>Задание №1</h1>
+ * Реализуйте интерфейс {@link IStringRowsListSorter}.
+ *
+ * <p>Мы будем обращать внимание в первую очередь на структуру кода и владение стандартными средствами java.</p>
+ */
+public class Task1Impl implements IStringRowsListSorter {
+    // ваша реализация должна работать, как singleton. даже при использовании из нескольких потоков.
+    private Task1Impl() {
+    }
+    private static final IStringRowsListSorter INSTANCE = new Task1Impl();
+    private static final SortList SORT_LIST = new SortList();
+    public static IStringRowsListSorter getInstance(){
+        return INSTANCE;
+    }
+
+    @Override
+    public void sort(final List<String[]> rows, final int columnIndex) {
+        // напишите здесь свою реализацию. Мы ждем от вас хорошо структурированного, документированного и понятного кода.
+        //Вызываем метод сортировки слияния. С параметрами исходный не сортированный лист, начальный \ конечный индекс строки,
+        // индекс в строке по которому производится сортировка.
+        SORT_LIST.mergeSort(rows, 0, rows.size() - 1, columnIndex);
+    }
+
+}
